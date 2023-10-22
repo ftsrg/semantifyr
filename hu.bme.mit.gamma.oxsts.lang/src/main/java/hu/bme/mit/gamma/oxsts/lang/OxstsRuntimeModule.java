ef6f200165bc7c097f963866674a3f71dc4ab427
Armin Zavada <zavadaarmin@edu.bme.hu>
2023-10-22 16:27:00 +0200

    Added :: as FQN separator

--- hu.bme.mit.gamma.oxsts.lang/src/main/java/hu/bme/mit/gamma/oxsts/lang/OxstsRuntimeModule.java
+++ hu.bme.mit.gamma.oxsts.lang/src/main/java/hu/bme/mit/gamma/oxsts/lang/OxstsRuntimeModule.java
@@ -4,8 +4,21 @@
 package hu.bme.mit.gamma.oxsts.lang;
 
 
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class OxstsRuntimeModule extends AbstractOxstsRuntimeModule {
+    public Class<? extends IQualifiedNameConverter> bindIQualifiedNameConverter() {
+        return OxstsQualifiedNameConverter.class;
+    }
+}
+
+class OxstsQualifiedNameConverter extends IQualifiedNameConverter.DefaultImpl {
+    @Override
+    public String getDelimiter() {
+        return "::";
+    }
 }
