b4d4660f71b146a0fb5b2b99038b66de1d9ade37
Armin Zavada <zavadaarmin@edu.bme.hu>
2024-03-26 20:49:56 +0100

    Model reader now correctly detects model errors

--- engine/src/main/kotlin/reader/OxstsReader.kt
+++ engine/src/main/kotlin/reader/OxstsReader.kt
@@ -4,6 +4,7 @@ import hu.bme.mit.gamma.oxsts.model.oxsts.OxstsPackage
 import hu.bme.mit.gamma.oxsts.model.oxsts.Package
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.xtext.EcoreUtil2
 import org.eclipse.xtext.resource.XtextResource
 import org.eclipse.xtext.util.CancelIndicator
 import org.eclipse.xtext.validation.CheckMode
@@ -33,13 +34,15 @@ class OxstsReader(
 
         for (file in inputFile.walkFiles().filter { it.extension == "oxsts" }) {
             val resource = resourceSet.getResource(URI.createURI(file.path), true)
-            //resource.load(emptyMap<Any, Any>())
-//            if (resource.errors.any()) {
-//                println(resource.errors)
-//            }
-//            if (resource.warnings.any()) {
-//                println(resource.warnings)
-//            }
+            resource.load(emptyMap<Any, Any>())
+            EcoreUtil2.resolveAll(resource)
+            if (resource.errors.any()) {
+                println(resource.errors)
+                error("Errors found in the model: {${resource.errors.joinToString(",\n")}}")
+            }
+            if (resource.warnings.any()) {
+                println(resource.warnings)
+            }
             val validator = (resource as XtextResource).resourceServiceProvider.resourceValidator
             val issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl)
             for (issue in issues) {
