49ceb743cbc77c3131ac3b6ff5fdbfddd39a7b42
Armin Zavada <zavadaarmin@edu.bme.hu>
2024-05-16 17:05:26 +0200

    Reader can now read library models as well

--- engine/src/main/kotlin/reader/OxstsReader.kt
+++ engine/src/main/kotlin/reader/OxstsReader.kt
@@ -3,6 +3,7 @@ package hu.bme.mit.gamma.oxsts.engine.reader
 import hu.bme.mit.gamma.oxsts.model.oxsts.OxstsPackage
 import hu.bme.mit.gamma.oxsts.model.oxsts.Package
 import org.eclipse.emf.common.util.URI
+import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import org.eclipse.xtext.EcoreUtil2
 import org.eclipse.xtext.resource.XtextResource
@@ -18,23 +19,35 @@ fun prepareOxsts() {
 }
 
 class OxstsReader(
-    val inputDirectory: String
+    private val inputDirectory: String,
+    private val libraryDirectory: String = ""
 ) {
     private val resourceSet = ResourceSetImpl()
+    private val userResources = mutableListOf<Resource>()
 
     val rootElements
-        get() = resourceSet.resources.flatMap { it.contents }.map { it as Package }
+        get() = userResources.flatMap { it.contents }.map { it as Package }
 
     init {
         resourceSet.loadOptions[XtextResource.OPTION_ENCODING] = "UTF-8"
     }
 
     fun read() {
+        if (libraryDirectory.isNotBlank()) {
+            val libraryFile = File(libraryDirectory)
+
+            for (file in libraryFile.walkFiles().filter { it.extension == "oxsts" }) {
+                val resource = resourceSet.getResource(URI.createURI(file.path), true)
+                resource.load(emptyMap<Any, Any>())
+            }
+        }
+
         val inputFile = File(inputDirectory)
 
         for (file in inputFile.walkFiles().filter { it.extension == "oxsts" }) {
             val resource = resourceSet.getResource(URI.createURI(file.path), true)
             resource.load(emptyMap<Any, Any>())
+            userResources += resource
         }
 
         for (resource in resourceSet.resources) {
