7aab5019ecc0e0041a74792cb61ead6d4f6b8a4f
Armin Zavada <zavadaarmin@edu.bme.hu>
2024-10-06 22:10:01 +0200

    #13 Removed bind configuration

--- subprojects/oxsts.lang.ide/src/main/java/hu/bme/mit/semantifyr/oxsts/lang/ide/OxstsIdeModule.java
+++ subprojects/oxsts.lang.ide/src/main/java/hu/bme/mit/semantifyr/oxsts/lang/ide/OxstsIdeModule.java
@@ -6,19 +6,9 @@
 
 package hu.bme.mit.semantifyr.oxsts.lang.ide;
 
-import com.google.inject.Binder;
-import org.eclipse.xtext.ide.server.*;
-
 /**
  * Use this class to register ide components.
  */
 public class OxstsIdeModule extends AbstractOxstsIdeModule {
-    @Override
-    public void configure(Binder binder) {
-        super.configure(binder);
 
-        // Bind the required interfaces to their implementations
-        binder.bind(IProjectDescriptionFactory.class).to(DefaultProjectDescriptionFactory.class);
-        binder.bind(IMultiRootWorkspaceConfigFactory.class).to(MultiRootWorkspaceConfigFactory.class);
-    }
 }
