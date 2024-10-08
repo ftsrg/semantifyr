6d1505151019b8fc38ac191c71b9adaaae477d9a
Armin Zavada <zavadaarmin@edu.bme.hu>
2024-10-08 16:45:42 +0200

    #13 Added timeout to verification CLI

--- subprojects/semantifyr/src/main/kotlin/commands/BaseVerifyCommand.kt
+++ subprojects/semantifyr/src/main/kotlin/commands/BaseVerifyCommand.kt
@@ -3,18 +3,22 @@ package hu.bme.mit.semantifyr.oxsts.semantifyr.commands
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.long
 import hu.bme.mit.semantifyr.oxsts.semantifyr.theta.ThetaExecutor
 import org.slf4j.Logger
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 abstract class BaseVerifyCommand(name: String) : CliktCommand(name) {
 
     abstract val logger: Logger
 
     val model by argument().file(mustExist = true, canBeFile = true, canBeDir = false)
+    val timeout by option().long().default(5).help("Timeout in minutes")
     val thetaVersion by option().default("6.5.2")
     val thetaConfiguration by option().multiple(
         default = listOf(
@@ -33,6 +37,8 @@ abstract class BaseVerifyCommand(name: String) : CliktCommand(name) {
         val thetaExecutor = ThetaExecutor(
             thetaVersion,
             thetaConfiguration,
+            timeout,
+            TimeUnit.MINUTES
         )
 
         val workingDirectory = xstsFile.parentFile.absolutePath
