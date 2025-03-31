68fe1113d6c1e27966cf395e1e911ac2514233b8
Ármin Zavada <armin.zavada@outlook.com>
2025-03-31 15:06:38 +0200

    Ignore non-oxsts packages for now

--- subprojects/semantifyr-vscode/build.gradle.kts
+++ subprojects/semantifyr-vscode/build.gradle.kts
@@ -25,11 +25,11 @@ val distributionClasspath by configurations.creating {
 
 dependencies {
     distributionClasspath(project(":oxsts.lang.ide", configuration = "distributionOutput"))
-    distributionClasspath(project(":xsts.lang.ide", configuration = "distributionOutput"))
-    distributionClasspath(project(":cex.lang.ide", configuration = "distributionOutput"))
-    distributionClasspath(project(":gamma.lang.ide", configuration = "distributionOutput"))
-    distributionClasspath(project(":semantifyr", configuration = "distributionOutput"))
-    distributionClasspath(project(":gamma-frontend", configuration = "distributionOutput"))
+//    distributionClasspath(project(":xsts.lang.ide", configuration = "distributionOutput"))
+//    distributionClasspath(project(":cex.lang.ide", configuration = "distributionOutput"))
+//    distributionClasspath(project(":gamma.lang.ide", configuration = "distributionOutput"))
+//    distributionClasspath(project(":semantifyr", configuration = "distributionOutput"))
+//    distributionClasspath(project(":gamma-frontend", configuration = "distributionOutput"))
 }
 
 val cloneDistribution by tasks.registering(Sync::class) {
