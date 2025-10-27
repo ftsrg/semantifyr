2c445764cc2ae773b264ef1c618140021c45871f
Armin Zavada <zavadaarmin@edu.bme.hu>
2025-10-27 08:09:11 +0100

    Simplified Semantifyr loader

--- subprojects/semantics/src/main/kotlin/reader/SemantifyrLoader.kt
+++ subprojects/semantics/src/main/kotlin/reader/SemantifyrLoader.kt
@@ -20,7 +20,6 @@ import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.ResourceSet
 import org.eclipse.xtext.EcoreUtil2
 import org.eclipse.xtext.diagnostics.Severity
-import org.eclipse.xtext.resource.IResourceFactory
 import org.eclipse.xtext.resource.XtextResourceSet
 import org.eclipse.xtext.util.CancelIndicator
 import org.eclipse.xtext.validation.CheckMode
@@ -30,10 +29,10 @@ import kotlin.io.path.absolutePathString
 
 class SemantifyrModelContext(
     val resourceSet: ResourceSet,
-    val resources: List<Resource>
+    val modelResource: List<Resource>
 ) {
     fun streamClasses(): Sequence<ClassDeclaration> {
-        return resources.asSequence().mapNotNull {
+        return modelResource.asSequence().mapNotNull {
             it.contents.first() as? OxstsModelPackage
         }.flatMap {
             it.declarations
@@ -49,37 +48,21 @@ class SemantifyrLoader {
     private lateinit var resourceSetProvider: Provider<XtextResourceSet>
 
     @Inject
-    private lateinit var resourceFactory: IResourceFactory
-
-    @Inject
     private lateinit var resourceValidator: IResourceValidator
 
     @Inject
     private lateinit var libraryAdapterFinder: LibraryAdapterFinder
 
-    private val extraPaths = mutableListOf<Path>()
-
     private fun createResourceSet(): XtextResourceSet {
         val resourceSet = resourceSetProvider.get()
-        val libraryAdapter = libraryAdapterFinder.getOrInstall(resourceSet)
-        libraryAdapter.setAdditionalPaths(extraPaths)
+        libraryAdapterFinder.getOrInstall(resourceSet)
         return resourceSet
     }
 
     private fun loadFile(resourceSet: ResourceSet, path: Path): Resource {
         logger.info { "Reading file: $path" }
-        val resource = resourceFactory.createResource(URI.createFileURI(path.absolutePathString()))
-        resource.load(mapOf<Any, Any>())
-        resourceSet.resources += resource
+        val resource = resourceSet.getResource(URI.createFileURI(path.absolutePathString()), true)
         return resource
-    }
-
-    fun extraPaths(libraryPaths: List<Path>) {
-        extraPaths.addAll(libraryPaths)
-    }
-
-    fun extraPath(libraryPath: Path) {
-        extraPaths.add(libraryPath)
     }
 
     fun loadStandaloneModelContext(model: Path): SemantifyrModelContext {
@@ -100,7 +83,6 @@ class SemantifyrLoader {
     }
 
     private fun resolveAndValidate(resourceSet: ResourceSet) {
-        libraryAdapterFinder.getOrInstall(resourceSet).loadLibraryResources()
         EcoreUtil2.resolveAll(resourceSet)
         for (resource in resourceSet.resources) {
             validateResource(resource)
@@ -152,57 +134,5 @@ class SemantifyrLoader {
             }
         }
     }
-
-//    fun loadDirectoryModel(directory: Path): List<Resource> {
-//        val file = directory.toFile()
-//        val resourceSet = createResourceSet()
-//    }
-
-//    fun readDirectory(modelDirectory: String) {
-//        logger.info { "Reading library files from $libraryDirectory" }
-//
-//        val libraryFiles = File(libraryDirectory).walkFiles().filter {
-//            it.extension == "oxsts"
-//        }
-//
-//        for (file in libraryFiles) {
-//            readFile(file)
-//        }
-//
-//        logger.info { "Reading model files from $modelDirectory" }
-//
-//        val modelFiles = File(modelDirectory).walkFiles().filter {
-//            it.extension == "oxsts"
-//        }
-//
-//        for (file in modelFiles) {
-//            userResources += readFile(file)
-//        }
-//
-//        validateAndLoadResourceSet(resourceSet)
-//    }
-//
-//    fun readModel(modelPath: String) {
-//        logger.info { "Reading library files from $libraryDirectory" }
-//
-//        val modelFile = File(modelPath)
-//        val modelAbsolutePath = modelFile.absolutePath
-//
-//        val libraryFiles = File(libraryDirectory).walkFiles().filter {
-//            it.extension == "oxsts"
-//        }.filterNot {
-//            it.absolutePath == modelAbsolutePath
-//        }
-//
-//        for (file in libraryFiles) {
-//            readFile(file)
-//        }
-//
-//        logger.info("Reading model file")
-//
-//        userResources += readFile(modelFile)
-//
-//        validateAndLoadResourceSet(resourceSet)
-//    }
 
 }
