6a41473c4a5f7b7cd15b077c2242b9f043660266
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-03-30 04:34:52 +0200

    Use consistent JVM parameters

--- subprojects/backends/theta/theta-wrapper/scripts/theta-xsts-cli.cmd
+++ subprojects/backends/theta/theta-wrapper/scripts/theta-xsts-cli.cmd
@@ -4,7 +4,7 @@ set SCRIPTDIR=%~dp0
 set PATH=%SCRIPTDIR%lib;%PATH%
 set VERIFIER_NAME=Theta
 if "%1"=="--version" (
-    java -Xss512M -Xmx4G "-Djava.library.path=%SCRIPTDIR%lib" -jar "%SCRIPTDIR%jars\theta-xsts-cli.jar" CEGAR --version
+    java "-Djava.library.path=%SCRIPTDIR%lib" -jar "%SCRIPTDIR%jars\theta-xsts-cli.jar" CEGAR --version
     exit /b %ERRORLEVEL%
 )
-java -Xss512M -Xmx4G "-Djava.library.path=%SCRIPTDIR%lib" -jar "%SCRIPTDIR%jars\theta-xsts-cli.jar" %*
+java -Xss512M -Xms1G -Xmx4G -XX:+UseG1GC -XX:+UseTransparentHugePages "-Djava.library.path=%SCRIPTDIR%lib" -jar "%SCRIPTDIR%jars\theta-xsts-cli.jar" %*
