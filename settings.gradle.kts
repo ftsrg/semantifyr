e2894e5a5b9466883d9337b845e3437ee125a206
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-11 06:34:22 +0200

    Register server projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,12 @@ rootProject.children.forEach { project ->
     project.projectDir = file("subprojects/${project.name}")
 }
 
+include("semantifyr-live-backend")
+project(":semantifyr-live-backend").projectDir = file("subprojects/semantifyr-live/backend")
+
+include("semantifyr-live-frontend")
+project(":semantifyr-live-frontend").projectDir = file("subprojects/semantifyr-live/frontend")
+
 fun includeDirectory(dirPath: String) {
     val dir = file(dirPath)
     val projects = dir.listFiles()?.filter { it.isDirectory } ?: emptyList()
