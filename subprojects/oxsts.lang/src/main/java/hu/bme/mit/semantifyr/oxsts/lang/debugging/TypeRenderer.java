936e2053afd8906611f9d9cb78ab46c5d2c27f4a
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-18 17:48:32 +0200

    Removed the Instance eclass

--- subprojects/oxsts.lang/src/main/java/hu/bme/mit/semantifyr/oxsts/lang/debugging/TypeRenderer.java
+++ subprojects/oxsts.lang/src/main/java/hu/bme/mit/semantifyr/oxsts/lang/debugging/TypeRenderer.java
@@ -10,13 +10,9 @@ import com.google.inject.Inject;
 import hu.bme.mit.semantifyr.oxsts.lang.OxstsStandaloneSetup;
 import hu.bme.mit.semantifyr.oxsts.lang.naming.NamingUtil;
 import hu.bme.mit.semantifyr.oxsts.lang.serializer.ExpressionSerializer;
-import hu.bme.mit.semantifyr.oxsts.model.oxsts.DomainDeclaration;
 import hu.bme.mit.semantifyr.oxsts.model.oxsts.Expression;
-import hu.bme.mit.semantifyr.oxsts.model.oxsts.Instance;
 import hu.bme.mit.semantifyr.oxsts.model.oxsts.NamedElement;
 import org.eclipse.emf.ecore.EObject;
-
-import java.util.ArrayList;
 
 public class TypeRenderer {
 
@@ -38,29 +34,12 @@ public class TypeRenderer {
         if (element instanceof NamedElement namedElement) {
             return NamingUtil.getName(namedElement);
         }
-        if (element instanceof Instance instance) {
-            return serialize(instance);
-        }
 
         return element.toString();
     }
 
     public static String render(EObject element) {
         return instance.serialize(element);
-    }
-
-    private static String serialize(Instance element) {
-        var list = new ArrayList<DomainDeclaration>();
-        list.add(element.getDomain());
-
-        while (element.getParent() != null && element.getParent().getDomain() != null) {
-            list.add(element.getParent().getDomain());
-            element = element.getParent();
-        }
-
-        var names = list.reversed().stream().map(NamingUtil::getName).toList();
-
-        return String.join(".", names);
     }
 
 }
