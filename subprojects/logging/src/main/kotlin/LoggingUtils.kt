d9984b4667ec01e3568f8227d19d540f71103ed5
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-18 23:05:19 +0200

    Added KDoc to logger

--- subprojects/logging/src/main/kotlin/LoggingUtils.kt
+++ subprojects/logging/src/main/kotlin/LoggingUtils.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 The Semantifyr Authors
+ * SPDX-FileCopyrightText: 2026 The Semantifyr Authors
  *
  * SPDX-License-Identifier: EPL-2.0
  */
@@ -9,34 +9,53 @@ package hu.bme.mit.semantifyr.logging
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * Creates a Logger with the current [javaClass] using a delegate property pattern.
+ * Usage: val logger by loggerFactory()
+ */
 fun <R : Any> R.loggerFactory(): Lazy<Logger> {
     return lazy { LoggerFactory.getLogger(javaClass) }
 }
 
+/**
+ * Inlined helper logging only if trace is enabled.
+ */
 inline fun Logger.trace(msgSupplier: () -> String) {
     if (this.isTraceEnabled) {
         trace(msgSupplier())
     }
 }
 
+/**
+ * Inlined helper logging only if debug is enabled.
+ */
 inline fun Logger.debug(msgSupplier: () -> String) {
     if (this.isDebugEnabled) {
         debug(msgSupplier())
     }
 }
 
+/**
+ * Inlined helper logging only if info is enabled.
+ */
 inline fun Logger.info(msgSupplier: () -> String) {
     if (this.isInfoEnabled) {
         info(msgSupplier())
     }
 }
 
+/**
+ * Inlined helper logging only if warn is enabled.
+ */
 inline fun Logger.warn(msgSupplier: () -> String) {
     if (this.isWarnEnabled) {
         warn(msgSupplier())
     }
 }
 
+/**
+ * Inlined helper logging only if error is enabled.
+ */
 inline fun Logger.error(msgSupplier: () -> String) {
     if (this.isErrorEnabled) {
         error(msgSupplier())
