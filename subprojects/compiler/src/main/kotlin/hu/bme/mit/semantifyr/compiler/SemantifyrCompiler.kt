63d60da2c813e0efd1e6b2003849fcfb78fe7b7e
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-20 20:48:15 +0200

    Updated the compiler api

--- subprojects/compiler/src/main/kotlin/hu/bme/mit/semantifyr/compiler/SemantifyrCompiler.kt
+++ subprojects/compiler/src/main/kotlin/hu/bme/mit/semantifyr/compiler/SemantifyrCompiler.kt
@@ -6,8 +6,10 @@
 
 package hu.bme.mit.semantifyr.compiler
 
+import com.google.inject.Injector
 import hu.bme.mit.semantifyr.compiler.pipeline.CompilationModule
 import hu.bme.mit.semantifyr.logging.debug
+import hu.bme.mit.semantifyr.logging.info
 import hu.bme.mit.semantifyr.logging.loggerFactory
 import hu.bme.mit.semantifyr.oxsts.lang.OxstsStandaloneSetup
 import hu.bme.mit.semantifyr.oxsts.model.oxsts.ClassDeclaration
@@ -19,24 +21,32 @@ import hu.bme.mit.semantifyr.compiler.pipeline.optimization.OptimizationConfig
 
 /**
  * The main Compiler entrypoint that runs the whole Semantifyr compilation pipeline on the input models.
- * The pipeline is instantiated in a fresh [com.google.inject.Injector] on each call.
  */
-class SemantifyrCompiler @JvmOverloads constructor(
+class SemantifyrCompiler(
+    private val injector: Injector,
     private val artifactConfig: ArtifactConfig,
     private val optimizationConfig: OptimizationConfig = OptimizationConfig.DEFAULT,
 ) : AutoCloseable {
 
-    private val injector = OxstsStandaloneSetup().createInjectorAndDoEMFRegistration()
+    @JvmOverloads
+    constructor(
+        artifactConfig: ArtifactConfig,
+        optimizationConfig: OptimizationConfig = OptimizationConfig.DEFAULT,
+    ) : this(OxstsStandaloneSetup().createInjectorAndDoEMFRegistration(), artifactConfig, optimizationConfig)
 
     private val logger by loggerFactory()
 
+    init {
+        logger.info { "Compiler artifacts will be written to: ${artifactConfig.outputDirectory.toAbsolutePath()}" }
+    }
+
     fun compile(classDeclaration: ClassDeclaration): FlattenedCompilationContext {
-        logger.debug { "Compiling class '${classDeclaration.name}'" }
+        logger.info { "Compiling class '${classDeclaration.name}'" }
         return freshPipeline().compileDeflated(classDeclaration)
     }
 
     fun compile(inlinedOxsts: InlinedOxsts): FlattenedCompilationContext {
-        logger.debug { "Compiling inlined oxsts of '${inlinedOxsts.classDeclaration.name}'" }
+        logger.info { "Compiling inlined oxsts of '${inlinedOxsts.classDeclaration.name}'" }
         return freshPipeline().compileDeflated(inlinedOxsts)
     }
 
