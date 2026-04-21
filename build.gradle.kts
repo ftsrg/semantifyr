d7c3e7c6039a9b1fc1dbeb70043d98bd0deea754
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-21 17:48:52 +0200

    Added jacoco aggregation

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,30 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.bmuschko.docker) apply false
+    `jacoco-report-aggregation`
+}
+
+val jvmConventionIds = setOf(
+    "hu.bme.mit.semantifyr.gradle.conventions.jvm",
+)
+
+dependencies {
+    subprojects.forEach { subproject ->
+        jvmConventionIds.forEach { conventionId ->
+            subproject.plugins.withId(conventionId) {
+                add("jacocoAggregation", subproject)
+            }
+        }
+    }
+}
+
+reporting {
+    reports {
+        val testCodeCoverageReport by creating(JacocoCoverageReport::class) {
+            testSuiteName = "test"
+        }
+        val verificationTestCodeCoverageReport by creating(JacocoCoverageReport::class) {
+            testSuiteName = "verificationTest"
+        }
+    }
 }
