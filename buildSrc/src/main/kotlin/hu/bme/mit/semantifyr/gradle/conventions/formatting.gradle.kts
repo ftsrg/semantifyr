2f0f30499cbbfad28253c0d6433936fd30fa3125
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-25 13:32:57 +0200

    Using palantir java formatter

--- buildSrc/src/main/kotlin/hu/bme/mit/semantifyr/gradle/conventions/formatting.gradle.kts
+++ buildSrc/src/main/kotlin/hu/bme/mit/semantifyr/gradle/conventions/formatting.gradle.kts
@@ -16,14 +16,20 @@ val libs = the<LibrariesForLibs>()
 
 spotless {
     val ktlintVersion = libs.versions.ktlint.get()
-    val googleJavaFormatVersion = libs.versions.googleJavaFormat.get()
+    val palantirVersion = libs.versions.palantirJavaFormat.get()
 
     kotlin {
         target("src/**/*.kt")
         ktlint(ktlintVersion).editorConfigOverride(
             mapOf(
                 "ktlint_standard_no-empty-file" to "disabled",
+                "ktlint_standard_chain-method-continuation" to "disabled",
+                // "ktlint_standard_binary-expression-wrapping" to "disabled",
+                "ktlint_standard_no-empty-first-line-at-start-in-class-body" to "disabled",
+                "ktlint_standard_no-blank-line-before-rbrace" to "disabled",
+                "ktlint_standard_unary-op-spacing" to "disabled",
                 "ij_kotlin_packages_to_use_import_on_demand" to "java.util.*,kotlinx.android.synthetic.**,io.ktor.**",
+                "ij_kotlin_line_break_after_multiline_when_entry" to "false",
                 "ktlint_function_signature_rule_force_multiline_when_parameter_count_greater_or_equal_than" to "3",
             ),
         )
@@ -37,6 +43,6 @@ spotless {
     java {
         target("src/**/*.java")
         targetExclude("**/xtext-gen/**", "**/emf-gen/**", "**/build/**")
-        googleJavaFormat(googleJavaFormatVersion).aosp()
+        palantirJavaFormat(palantirVersion)
     }
 }
