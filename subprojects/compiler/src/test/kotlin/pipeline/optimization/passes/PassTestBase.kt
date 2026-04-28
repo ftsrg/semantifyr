b72e91927764187f6655b1b034c706ed0418dd45
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-04-28 20:02:27 +0200

    Added optimization dump helper

--- subprojects/compiler/src/test/kotlin/pipeline/optimization/passes/PassTestBase.kt
+++ subprojects/compiler/src/test/kotlin/pipeline/optimization/passes/PassTestBase.kt
@@ -9,6 +9,7 @@ package hu.bme.mit.semantifyr.compiler.pipeline.optimization.passes
 import com.google.inject.Inject
 import com.google.inject.Injector
 import hu.bme.mit.semantifyr.compiler.pipeline.CompilationModule
+import hu.bme.mit.semantifyr.compiler.pipeline.CompilationRequest
 import hu.bme.mit.semantifyr.compiler.pipeline.artifact.ArtifactConfig
 import hu.bme.mit.semantifyr.compiler.pipeline.context.CreatedCompilationContext
 import hu.bme.mit.semantifyr.compiler.pipeline.context.EvaluableCompilationContext
@@ -18,6 +19,7 @@ import hu.bme.mit.semantifyr.compiler.pipeline.optimization.Analysis
 import hu.bme.mit.semantifyr.compiler.pipeline.optimization.AnalysisManager
 import hu.bme.mit.semantifyr.compiler.pipeline.optimization.OptimizationConfig
 import hu.bme.mit.semantifyr.compiler.pipeline.optimization.Pass
+import hu.bme.mit.semantifyr.compiler.pipeline.optimization.PassResult
 import hu.bme.mit.semantifyr.compiler.pipeline.optimization.verifyInjectedDependenciesAreBound
 import hu.bme.mit.semantifyr.compiler.pipeline.utils.normalizedFixtureSource
 import hu.bme.mit.semantifyr.compiler.pipeline.utils.serializeFormatted
@@ -72,12 +74,16 @@ abstract class PassTestBase {
 
         val child = injector.createChildInjector(
             CompilationModule(
-                ArtifactConfig.none(Files.createTempDirectory("pass-test-")),
+                ArtifactConfig.NONE,
                 OptimizationConfig.ALL,
             ),
         )
 
-        val passClassName = withCompilationScopeBlocking(actual.inlinedOxsts) {
+        val request = CompilationRequest(
+            inlinedOxsts = actual.inlinedOxsts,
+            outputDirectory = Files.createTempDirectory("pass-test-"),
+        )
+        val passClassName = withCompilationScopeBlocking(request) {
             val analyses = analysisClasses.map {
                 child.getInstance(it)
             }
@@ -92,6 +98,47 @@ abstract class PassTestBase {
         assertThat(actualText)
             .describedAs("Pass $passClassName should have rewritten the input to the expected form")
             .isEqualTo(expectedText)
+    }
+
+    protected fun dumpOptimizerPipeline(
+        source: String,
+        passClasses: List<Class<out Pass<EvaluableCompilationContext>>>,
+        analysisClasses: List<Class<out Analysis<*>>>,
+        maxRounds: Int = 10,
+        log: (String) -> Unit = ::println,
+    ) {
+        val actual = compile(source)
+        val child = injector.createChildInjector(
+            CompilationModule(
+                ArtifactConfig.NONE,
+                OptimizationConfig.ALL,
+            ),
+        )
+        val request = CompilationRequest(
+            inlinedOxsts = actual.inlinedOxsts,
+            outputDirectory = Files.createTempDirectory("dump-optimizer-"),
+        )
+        withCompilationScopeBlocking(request) {
+            val passes = passClasses.map { child.getInstance(it) }
+            val analyses = analysisClasses.map { child.getInstance(it) }
+            val analysisManager = AnalysisManager(analyses)
+            log(">>> START\n${serializer.serializeFormatted(actual.inlinedOxsts)}")
+            var round = 0
+            var iteration = true
+            while (iteration && round < maxRounds) {
+                round++
+                iteration = false
+                for (pass in passes) {
+                    val name = pass::class.simpleName
+                    val result = pass.run(actual.context, analysisManager)
+                    if (result is PassResult.Changed) {
+                        iteration = true
+                        analysisManager.invalidateExcept(result.preserved)
+                        log(">>> round=$round after $name CHANGED\n${serializer.serializeFormatted(actual.inlinedOxsts)}")
+                    }
+                }
+            }
+        }
     }
 
     private class SingleRootInstanceTree(
