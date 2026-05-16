93c57812799d3b6c4747a14c12b84968a135c91e
Armin Zavada <zavadaarmin@edu.bme.hu>
2026-05-16 02:14:36 +0200

    Always download node

--- buildSrc/src/main/kotlin/hu/bme/mit/semantifyr/gradle/conventions/nodejs.gradle.kts
+++ buildSrc/src/main/kotlin/hu/bme/mit/semantifyr/gradle/conventions/nodejs.gradle.kts
@@ -11,11 +11,7 @@ plugins {
     id("com.github.node-gradle.node")
 }
 
-val ciEnv = providers.environmentVariable("CI").map {
-    it.equals("true", ignoreCase = true)
-}.orElse(false)
-
 node {
     version = "22.14.0"
-    download = ciEnv.map { !it }
+    download = true
 }
